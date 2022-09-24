735380b2b46dbfbb77fa463e4fc3fdacd1a0ae6b
KadoreK <haltaskadir@gmail.com>
2022-09-24 23:30:00 +0300

    Some products added

--- Gross/src/app/product/product.component.ts
+++ Gross/src/app/product/product.component.ts
@@ -8,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
 
   constructor() { }
+  title="Ürün Listesi"
+  products:any[]=
+  [
+    {id:1,name:"Asus ZenBook 13",price:25000,categoryId:1,description:"asus zen book 13 inch wonder full"},
+    {id:3,name:"Toshiba Satalite 15,6",price:27000,categoryId:1,description:"toshiba notebook is wonder full"},
+    {id:10,name:"Poco X3 NFC",price:6000,categoryId:3,description:"POCO X3 NFC is wonder full"}
+  ]
 
   ngOnInit(): void {
   }

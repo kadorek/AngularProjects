faf0fafff89ab38788266c370d8274f9ebab3371
KadoreK <haltaskadir@hotmail.com>
2022-09-27 17:17:47 +0300

    product class defined and added

--- Gross/src/app/product/product.component.ts
+++ Gross/src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
 
 @Component({
   selector: 'app-product',
@@ -9,11 +10,11 @@ export class ProductComponent implements OnInit {
 
   constructor() { }
   title="Ürün Listesi"
-  products:any[]=
+  products:Product[]=
   [
-    {id:1,name:"Asus ZenBook 13",price:25000,categoryId:1,description:"asus zen book 13 inch wonder full"},
-    {id:3,name:"Toshiba Satalite 15,6",price:27000,categoryId:1,description:"toshiba notebook is wonder full"},
-    {id:10,name:"Poco X3 NFC",price:6000,categoryId:3,description:"POCO X3 NFC is wonder full"}
+    {id:1,name:"Asus ZenBook 13",price:25000,categoryId:1,description:"asus zen book 13 inch wonder full",imageUrl:"https://images.unsplash.com/photo-1575024357670-2b5164f470c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"},
+    {id:3,name:"Toshiba Satalite 15,6",price:27000,categoryId:1,description:"toshiba notebook is wonder full",imageUrl:"https://images.unsplash.com/photo-1575024357670-2b5164f470c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"},
+    {id:10,name:"Poco X3 NFC",price:6000,categoryId:3,description:"POCO X3 NFC is wonder full",imageUrl:"https://images.unsplash.com/photo-1575024357670-2b5164f470c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"}
   ]
 
   ngOnInit(): void {

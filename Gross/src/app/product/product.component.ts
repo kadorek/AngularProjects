f5140685986c176d0cc8eea0de3770376f546b01
KadoreK <haltaskadir@gmail.com>
2022-09-28 22:55:34 +0300

    custom pipe creation and ngmodule usage

--- Gross/src/app/product/product.component.ts
+++ Gross/src/app/product/product.component.ts
@@ -9,7 +9,8 @@ import { Product } from './product';
 export class ProductComponent implements OnInit {
 
   constructor() { }
-  title="Ürün Listesi"
+  title="Ürün Listesi";
+  filterText="";
   products:Product[]=
   [
     {id:1,name:"Asus ZenBook 13",price:25000,categoryId:1,description:"asus zen book 13 inch wonder full",imageUrl:"https://images.unsplash.com/photo-1575024357670-2b5164f470c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"},

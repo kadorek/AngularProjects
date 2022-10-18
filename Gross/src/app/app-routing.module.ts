ef1eb48186347a27d883b59f97fb7d01e34de4ed
KadoreK <haltaskadir@gmail.com>
2022-10-19 01:19:37 +0300

    added routing path for product adding techniques

--- Gross/src/app/app-routing.module.ts
+++ Gross/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
   {path:"product",component:ProductComponent},
+  {path:"product-add-classic",component:ProductComponent},
+  {path:"product-add-reactive",component:ProductComponent},
   {path:"",redirectTo:"product",pathMatch:"full"},
   {path:"category/:categoryName",component:ProductComponent}
 ];
